51ec463f3cd23952f54ccc6dd0c1d404449c3779
EthanYidong <ethanyidong@gmail.com>
2020-11-15 09:13:37 -0800

    v0.2: Clippy fixes

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,21 @@
 use clap::Clap;
 
 use hyper::{
-    Body, Request, Response, Server, Method, StatusCode,
     service::{make_service_fn, service_fn},
+    Body, Method, Request, Response, Server, StatusCode,
 };
 
 use reqwest::{Client, Url};
 
 use serde::Deserialize;
 
-use regex::{Regex, Captures};
+use regex::{Captures, Regex};
 
-use std::path::PathBuf;
-use std::convert::Infallible;
-use std::net::{IpAddr, SocketAddr};
 use std::collections::HashMap;
+use std::convert::Infallible;
 use std::fs::read_to_string;
+use std::net::{IpAddr, SocketAddr};
+use std::path::PathBuf;
 use std::sync::Arc;
 
 #[derive(Clap)]
@@ -32,7 +32,7 @@ struct CliOpts {
     port: u16,
     /// Override config vars with env vars
     #[clap(short = 'o', long = "override")]
-    use_env: bool
+    use_env: bool,
 }
 
 #[derive(Deserialize)]
@@ -60,12 +60,8 @@ struct Replacement {
 #[derive(Deserialize)]
 #[serde(untagged)]
 enum FileLocation {
-    Local {
-        path: String,
-    },
-    External {
-        url: String,
-    }
+    Local { path: String },
+    External { url: String },
 }
 
 async fn shutdown_signal() {
@@ -75,21 +71,23 @@ async fn shutdown_signal() {
         .expect("failed to install CTRL+C signal handler");
 }
 
-async fn server(file_contents: Arc<HashMap<String, String>>, req: Request<Body>) -> Result<Response<Body>, Infallible> {
+async fn server(
+    file_contents: Arc<HashMap<String, String>>,
+    req: Request<Body>,
+) -> Result<Response<Body>, Infallible> {
     let mut response = Response::new(Body::empty());
 
-    match req.method() {
-        &Method::GET => {
+    match *req.method() {
+        Method::GET => {
             if file_contents.contains_key(&req.uri().path()[1..]) {
                 *response.body_mut() = Body::from(file_contents[&req.uri().path()[1..]].clone());
-            }
-            else {
+            } else {
                 *response.status_mut() = StatusCode::NOT_FOUND;
             }
-        },
+        }
         _ => {
             *response.status_mut() = StatusCode::NOT_FOUND;
-        },
+        }
     };
 
     Ok(response)
@@ -103,7 +101,8 @@ async fn main() {
 
     // Read config data from config file
     let config_str = read_to_string(opts.config).expect("Error reading config");
-    let Config{vars, files} = toml::from_str::<Config>(&config_str).expect("Error deserializing toml");
+    let Config { vars, files } =
+        toml::from_str::<Config>(&config_str).expect("Error deserializing toml");
 
     // Get file content
     let mut file_contents = HashMap::new();
@@ -114,38 +113,52 @@ async fn main() {
 
     for file in files {
         let (mut name, mut contents) = match file.location {
-            FileLocation::Local{path} => {
+            FileLocation::Local { path } => {
                 // Fetch file from local fs
                 let path = PathBuf::from(path);
-                let file_name = String::from(path.file_name().expect("Error getting file name").to_str().unwrap());
+                let file_name = String::from(
+                    path.file_name()
+                        .expect("Error getting file name")
+                        .to_str()
+                        .unwrap(),
+                );
                 let contents = read_to_string(&path).expect("Error reading file");
-                
+
                 (file_name, contents)
-            },
-            FileLocation::External{url} => {
+            }
+            FileLocation::External { url } => {
                 // Fetch file from url
                 let url = Url::parse(&url).expect("Invalid URL");
-                let file_name = String::from(url.path_segments().expect("Could not split path").next_back().expect("Error getting file name"));
+                let file_name = String::from(
+                    url.path_segments()
+                        .expect("Could not split path")
+                        .next_back()
+                        .expect("Error getting file name"),
+                );
                 let resp = client.get(url).send().await.expect("Error GETting url");
                 let contents = resp.text().await.expect("Invalid response body");
 
                 (file_name, contents)
-            },
+            }
         };
 
         for re in file.replace {
-            let to = regex.replace_all(&re.to, |caps: &Captures| {
-                if use_env {
-                    std::env::var(&caps[2].to_uppercase()).ok().map(|var| format!("{}{}", &caps[1], var))
-                } else {
-                    None
-                }.unwrap_or(
-                    match vars.get(&caps[2]) {
+            let to = regex
+                .replace_all(&re.to, |caps: &Captures| {
+                    if use_env {
+                        std::env::var(&caps[2].to_uppercase())
+                            .ok()
+                            .map(|var| format!("{}{}", &caps[1], var))
+                    } else {
+                        None
+                    }
+                    .unwrap_or(match vars.get(&caps[2]) {
                         Some(s) => format!("{}{}", &caps[1], s),
                         None => String::from(&caps[0]),
-                    }
-                )
-            }).replace(r"\{", "{").replace(r"\}", "}");
+                    })
+                })
+                .replace(r"\{", "{")
+                .replace(r"\}", "}");
             contents = contents.replace(&re.from, &to);
         }
 
@@ -162,7 +175,12 @@ async fn main() {
     let file_contents_arc = Arc::new(file_contents);
 
     // Server bind to SocketAddr from opts
-    let addr = SocketAddr::from((opts.host.parse::<IpAddr>().expect("Error parsing IP address"), opts.port));
+    let addr = SocketAddr::from((
+        opts.host
+            .parse::<IpAddr>()
+            .expect("Error parsing IP address"),
+        opts.port,
+    ));
 
     // Convert to hyper service
     let make_svc = make_service_fn(move |_conn| {
@@ -175,7 +193,9 @@ async fn main() {
     });
 
     // Run server
-    let server = Server::bind(&addr).serve(make_svc).with_graceful_shutdown(shutdown_signal());
+    let server = Server::bind(&addr)
+        .serve(make_svc)
+        .with_graceful_shutdown(shutdown_signal());
 
     // Wait for errors, print
     if let Err(e) = server.await {
